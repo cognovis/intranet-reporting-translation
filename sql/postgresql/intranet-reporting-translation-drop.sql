0f4debf3ba2b5c4ad5cb90544b7dfe8b32234a52
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2015-01-16 16:43:58 +0000

    - Translation Reporting:   - Fixed drop-script in order to delete reports

--- sql/postgresql/intranet-reporting-translation-drop.sql
+++ sql/postgresql/intranet-reporting-translation-drop.sql
@@ -8,4 +8,29 @@
 -- @author frank.bergmann@project-open.com
 
 
+
+-- Delete reports defined by this package
+create or replace function inline_0 ()
+returns integer as $body$
+declare
+	row			RECORD;
+	v_menu			integer;
+	v_reporting_menu 	integer;
+BEGIN
+	FOR row IN
+		select	report_id
+		from	im_reports
+		where	report_menu_id in (
+			select	menu_id
+			from	im_menus
+			where	package_name = 'intranet-reporting-translation'
+		)
+	LOOP
+		perform im_report__delete(row.report_id);
+	END LOOP;
+	return 0;
+end;$body$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
 select im_menu__del_module('intranet-reporting-translation');
